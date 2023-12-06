cc81b2f053fb5243bac3d89b3bc5158fce50ebdb
Yuji <116667889+Yujif1Aero@users.noreply.github.com>
2023-12-06 13:20:58 +0900

    Add files via upload

---
+++ CudaUnistalationWhenYouInstallByRunFile.f90
@@ -0,0 +1,15 @@
+ubuntu@lab-fnd-a1hs-002:~$ sudo sh cuda_12.1.0_530.30.02_linux.run
+===========
+= Summary =
+===========
+
+Driver:   Installed
+Toolkit:  Installed in /usr/local/cuda-12.1/
+
+Please make sure that
+ -   PATH includes /usr/local/cuda-12.1/bin
+ -   LD_LIBRARY_PATH includes /usr/local/cuda-12.1/lib64, or, add /usr/local/cuda-12.1/lib64 to /etc/ld.so.conf and run ldconfig as root
+
+To uninstall the CUDA Toolkit, run cuda-uninstaller in /usr/local/cuda-12.1/bin
+To uninstall the NVIDIA Driver, run nvidia-uninstall
+Logfile is /var/log/cuda-installer.log
